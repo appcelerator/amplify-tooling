c54f837118dca90b751fa30b24f677945643cc02
Chris Barber <chris@cb1inc.com>
2019-02-06 09:03:14 -0600

    fix(auth): Updated environment auth settings for new realm. Fixes CLI-36.

--- packages/amplify-cli-auth/src/commands/server-info.js
+++ packages/amplify-cli-auth/src/commands/server-info.js
@@ -3,16 +3,17 @@ export default {
 	name: 'server-info',
 	async action({ argv, console }) {
 		const { Auth } = await import('@axway/amplify-auth-sdk');
-		const { auth } = await import('@axway/amplify-cli-utils');
+		const { auth, loadConfig } = await import('@axway/amplify-cli-utils');
+		const config = loadConfig();
 
 		const params = auth.buildParams({
 			baseUrl:  argv.baseUrl,
 			clientId: argv.clientId,
 			env:      argv.env,
 			realm:    argv.realm
-		});
+		}, config);
 
-		const client = new Auth();
+		const client = new Auth(params);
 		const info = await client.serverInfo(params);
 		console.log(JSON.stringify(info, null, '  '));
 	}
