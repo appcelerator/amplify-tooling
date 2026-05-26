9249f0b312d190f5d85a991d98853ee39fb33c88
Nathan Boxhall-Burnett <nbb@axway.com>
2026-05-26 15:04:03 -0700

    Fix oclif hook unit tests

--- src/hooks/command_not_found/suggest.ts
+++ src/hooks/command_not_found/suggest.ts
@@ -6,10 +6,24 @@ import logger from '../../lib/logger.js';
 const { warn } = logger('hook:command_not_found:suggestions');
 
 const hook: Hook.CommandNotFound = async function ({ config, id, argv }) {
+	// Oclif does not include a -v shorthand for the default version flag, so let's add one manually here.
+	if (id === '-v' || argv?.includes('-v')) {
+		return this.log(config.userAgent);
+	}
+
 	const closestCommand: string = closest(id, config.commandIDs);
 	const dist = distance(id, closestCommand);
 	if (dist > 3) {
 		return this.error(`Command "${toConfiguredId(id, config)}" not found.`);
+	}
+
+	// Skip interactive prompt in non-TTY environments
+	if (!process.stdin.isTTY && process.env.AXWAY_TEST !== '2') {
+		let msg = `Command "${toConfiguredId(id, config)}" not found.`;
+		if (closestCommand) {
+			msg += ` Did you mean "${toConfiguredId(closestCommand, config)}"?`;
+		}
+		return this.error(msg);
 	}
 
 	warn('Unknown command detected, prompting for confirmation of closest match');
